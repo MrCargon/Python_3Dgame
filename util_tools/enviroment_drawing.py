e496e850d2ffb8d681222d99d0b7d3262a6153e4
MrCargon <xerleon1951@gmail.com>
2023-10-05 00:07:08 -0700

    Update_ with more descriptions

--- util_tools/enviroment_drawing.py
+++ util_tools/enviroment_drawing.py
@@ -5,9 +5,12 @@ from map_enviroment import mini_map
 
 class Drawing:
     def __init__(self,sc,sc_map) -> None:
+        # Screen and minimap surfaces
         self.sc = sc
         self.sc_map = sc_map
+        # Font for FPS
         self.font = pygame.font.SysFont('Arial', 36, bold=True)
+        # Texture images
         self.texture = {'default': pygame.image.load('res/img/default.png'),
                         '1': pygame.image.load('res/img/white_rock_with_blue_wall.png').convert(),
                         '2': pygame.image.load('res/img/brown_brick_wall.png').convert(),
@@ -15,34 +18,37 @@ class Drawing:
                         }
     
     def background(self,angle):
-        # New Sky
+        # Sky background from texture
         sky_offset = -5*math.degrees(angle) % WIDTH
         self.sc.blit(self.texture['S'],(sky_offset,0))
         self.sc.blit(self.texture['S'],(sky_offset - WIDTH,0))
         self.sc.blit(self.texture['S'],(sky_offset + WIDTH,0))
-        # Old Draw sky
-        # pygame.draw.rect(self.sc,SKYBLUE,(0,0,WIDTH,HALF_HEIGHT))
-        # Draw ground
+        # Ground rectangle
         pygame.draw.rect(self.sc,DARKGRAY,(0,HALF_HEIGHT,WIDTH,HALF_HEIGHT))
 
     def world(self,player_pos, player_angle):
+        # Call raycasting and draw world
         ray_casting(self.sc,player_pos,player_angle,self.texture)
 
     def fps(self, clock):
+        # Display frames per second
         display_fps = str(int(clock.get_fps()))
         render = self.font.render(display_fps, 0, RED)
         self.sc.blit(render, FPS_POS)
     
     def mini_map(self, player):
-        #
+        # Draw map background
         self.sc_map.fill(BLACK)
-        map_x, map_y = player.x // MAP_SCALE, player.y // MAP_SCALE
+        # Convert player position to map position
+        map_x = player.x // MAP_SCALE
+        map_y = player.y // MAP_SCALE
         # Draw player look at line
         pygame.draw.line(self.sc_map, YELLOW, (map_x, map_y), (map_x + 12 * math.cos(player.angle),
                                                  map_y + 12 * math.sin(player.angle)), 2)
-        # Draw player as curcle
+        # Draw player circle
         pygame.draw.circle(self.sc_map, RED, (int(map_x), int(map_y)), 5)
-        # Draw min map
+        # Draw map tiles
         for x, y in mini_map:
             pygame.draw.rect(self.sc_map, SANDY, (x, y, MAP_TILE, MAP_TILE))
+        # Blit map to screen
         self.sc.blit(self.sc_map, MAP_POS)
--- util_tools/vision_raycasting.py
+++ util_tools/vision_raycasting.py
@@ -7,40 +7,54 @@ def mapping(a,b):
     return (a//TILE_SIZE*TILE_SIZE,(b//TILE_SIZE)*TILE_SIZE)
 # New Ray cast System
 def ray_casting(sc,player_pos, player_angle,textures):
+    # Initialize to prevent unbound errors
+    texture_v = None
+    texture_h = None
+    # Current player position and angle
     ox, oy = player_pos
     xm, ym = mapping(ox, oy)
     cur_angle = player_angle - HALF_FOV
-    # Raycasts
+    # Ray casting loop
     for ray in range(NUM_RAYS):
+        # Calculate ray direction
         sin_a = math.sin(cur_angle)
         cos_a = math.cos(cur_angle)
+        # Fixdiv by zero errors
         sin_a = sin_a if sin_a else 0.000001
         cos_a = cos_a if cos_a else 0.000001
-        # vertical itterator function
+        # Vertical intersection
         x, dx = (xm + TILE_SIZE, 1) if cos_a >= 0 else (xm, -1)
         for i in range(0, WIDTH, TILE_SIZE):
+            # Calc depth and position
             depth_v = (x - ox) / cos_a
             yv = oy + depth_v * sin_a
+            # Map to tile
             tile_v = mapping(x + dx, yv)
+            # Check for map collision
             if tile_v in world_enviroment:
-                # vertical texture
+                # Set vertical texture on hit
                 texture_v = world_enviroment[tile_v]
-                # Brake ray
+                # Break vertical loop
                 break
+            # Increment x
             x += dx * TILE_SIZE
-        # horizontals itterator function
+        # Horizontal intersection
         y, dy = (ym + TILE_SIZE, 1) if sin_a >= 0 else (ym, -1)
         for i in range(0, HEIGHT, TILE_SIZE):
+            # Calc depth and position
             depth_h = (y - oy) / sin_a
             xh = ox + depth_h * cos_a
+            # Map to tile
             tile_h = mapping(xh, y + dy)
+            # Check for map collision
             if tile_h in world_enviroment:
-                # horizontal texture
+                # Set horizontal texture on hit
                 texture_h = world_enviroment[tile_h]
-                # Brake ray
+                # Break horizontal loop
                 break
+            # Increment y
             y += dy * TILE_SIZE
-        # Projection and Texture
+        # Choose vertical or horizontal hit
         depth, offset, texture = (depth_v, yv, texture_v) if depth_v < depth_h else (depth_h, xh, texture_h)
         # Wrap offset to texture width 
         offset = int(offset) % TILE_SIZE
@@ -53,5 +67,3 @@ def ray_casting(sc,player_pos, player_angle,textures):
         sc.blit(wall_column, (ray * SCALE, HALF_HEIGHT - proj_height // 2))
         #
         cur_angle += DELTA_ANGLE
-        # Return hits 
-        #return hit_points
